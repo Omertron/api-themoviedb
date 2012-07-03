619432fd6fb5f19460933485b0d4058543692465
Omertron <Stuart.Boston@Gmail.com>
2012-07-03 14:27:31 +0000

    Updated webbrowser to remove redundant check

--- themoviedbapi/src/main/java/com/moviejukebox/themoviedb/tools/WebBrowser.java
+++ themoviedbapi/src/main/java/com/moviejukebox/themoviedb/tools/WebBrowser.java
@@ -113,7 +113,8 @@ public final class WebBrowser {
                 if (in != null) {
                     in.close();
                 }
-                if (cnx != null && cnx instanceof HttpURLConnection) {
+                
+                if (cnx instanceof HttpURLConnection) {
                     ((HttpURLConnection) cnx).disconnect();
                 }
             }
