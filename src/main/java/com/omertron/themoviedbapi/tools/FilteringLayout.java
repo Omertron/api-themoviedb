3972984f268fb323c777fb40d05f6523a55e94d2
Omertron <>
2012-12-14 22:15:37 +0000

    Update licence to GPL

--- src/main/java/com/omertron/themoviedbapi/tools/FilteringLayout.java
+++ src/main/java/com/omertron/themoviedbapi/tools/FilteringLayout.java
@@ -1,11 +1,21 @@
 /*
  *      Copyright (c) 2004-2012 Stuart Boston
  *
- *      This software is licensed under a Creative Commons License
- *      See the LICENCE.txt file included in this package
+ *      This file is part of TheMovieDB API.
  *
- *      For any reuse or distribution, you must make clear to others the
- *      license terms of this work.
+ *      TheMovieDB API is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      TheMovieDB API is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with TheMovieDB API.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package com.omertron.themoviedbapi.tools;
 
