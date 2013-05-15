00063cc85d7ef4b939a1cef036629614c3a87e58
Stuart Boston <Stuart.Boston@Gmail.com>
2013-05-15 13:16:44 +0100

    Add reviews

---
+++ src/main/java/com/omertron/themoviedbapi/model/Reviews.java
@@ -0,0 +1,101 @@
+/*
+ *      Copyright (c) 2004-2013 Stuart Boston
+ *
+ *      This file is part of TheMovieDB API.
+ *
+ *      TheMovieDB API is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      TheMovieDB API is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with TheMovieDB API.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.omertron.themoviedbapi.model;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Stuart
+ */
+public class Reviews implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /*
+     * Logger
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(Reviews.class);
+    /*
+     * Properties
+     */
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("author")
+    private String author;
+    @JsonProperty("content")
+    private String content;
+    @JsonProperty("url")
+    private String url;
+
+    // <editor-fold defaultstate="collapsed" desc="Getter methods">
+    public String getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Setter methods">
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    // </editor-fold>
+
+    /**
+     * Handle unknown properties and print a message
+     *
+     * @param key
+     * @param value
+     */
+    @JsonAnySetter
+    public void handleUnknown(String key, Object value) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Unknown property: '").append(key);
+        sb.append("' value: '").append(value).append("'");
+        LOG.trace(sb.toString());
+    }
+}
