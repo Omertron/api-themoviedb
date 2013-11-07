3b1855d75cc916ab34e8a55b23eaf5e5fecca89c
Stuart Boston <Stuart.Boston@Gmail.com>
2013-11-07 09:50:06 +0000

    Remove unused imports

--- src/main/java/com/omertron/themoviedbapi/wrapper/AbstractWrapper.java
+++ src/main/java/com/omertron/themoviedbapi/wrapper/AbstractWrapper.java
@@ -25,8 +25,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public abstract class AbstractWrapper extends AbstractJsonMapping implements Serializable {
 
