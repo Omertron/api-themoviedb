d5c996aef62771e8f98a565afb93d202fdf9a58b
Stuart Boston <Stuart.Boston@Gmail.com>
2014-01-16 12:36:50 +0000

    Update copyright date

--- src/main/java/com/omertron/themoviedbapi/wrapper/WrapperChanges.java
+++ src/main/java/com/omertron/themoviedbapi/wrapper/WrapperChanges.java
@@ -1,13 +1,32 @@
+/*
+ *      Copyright (c) 2004-2014 Stuart Boston
+ *
+ *      This file is part of TheMovieDB API.
+ *
+ *      TheMovieDB API is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      TheMovieDB API is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with TheMovieDB API.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package com.omertron.themoviedbapi.wrapper;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.omertron.themoviedbapi.model.ChangeKeyItem;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
--- src/test/java/com/omertron/themoviedbapi/TestLogger.java
+++ src/test/java/com/omertron/themoviedbapi/TestLogger.java
@@ -1,20 +1,20 @@
 /*
- *      Copyright (c) 2004-2013 Stuart Boston
+ *      Copyright (c) 2004-2014 Stuart Boston
  *
- *      This file is part of the FanartTV API.
+ *      This file is part of TheMovieDB API.
  *
- *      The FanartTV API is free software: you can redistribute it and/or modify
+ *      TheMovieDB API is free software: you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
  *      the Free Software Foundation, either version 3 of the License, or
  *      any later version.
  *
- *      The FanartTV API is distributed in the hope that it will be useful,
+ *      TheMovieDB API is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *      GNU General Public License for more details.
  *
  *      You should have received a copy of the GNU General Public License
- *      along with the FanartTV API.  If not, see <http://www.gnu.org/licenses/>.
+ *      along with TheMovieDB API.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 package com.omertron.themoviedbapi;
