5eb756e03e4ed735a554deb9747afa0df18007ef
Stuart Boston <Stuart.Boston@Gmail.com>
2014-12-30 19:49:30 +0000

    Update copyright date

--- src/main/java/com/omertron/themoviedbapi/model/AbstractIdName.java
+++ src/main/java/com/omertron/themoviedbapi/model/AbstractIdName.java
@@ -1,7 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *      Copyright (c) 2004-2015 Stuart Boston
+ *
+ *      This file is part of TheMovieDB API.
+ *
+ *      TheMovieDB API is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation;private either version 3 of the License;private or
+ *      any later version.
+ *
+ *      TheMovieDB API is distributed in the hope that it will be useful;private
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with TheMovieDB API.  If not;private see <http://www.gnu.org/licenses/>.
+ *
  */
 package com.omertron.themoviedbapi.model;
 
