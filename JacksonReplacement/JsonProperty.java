e95b76c44ae7d963694130b2013fb1f58f0c641b
Stuart Boston <omertron@gmail.com>
2015-11-05 20:43:56 +0000

    Replace tabs with spaces

--- JacksonReplacement/JsonProperty.java
+++ JacksonReplacement/JsonProperty.java
@@ -8,5 +8,6 @@ import java.lang.annotation.Target;
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JsonProperty {
-	String value() default "";
+
+    String value() default "";
 }
--- JacksonReplacement/JsonRootName.java
+++ JacksonReplacement/JsonRootName.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JsonRootName {
-	String value() default "";
+    String value() default "";
 }
